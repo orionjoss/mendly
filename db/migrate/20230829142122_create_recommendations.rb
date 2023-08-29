cdd4874f0e4bf3d2559870a9ed00843b50860ba5
orionjoss <orionjoss@gmail.com>
2023-08-29 16:28:42 +0100

    Ädded model associations

--- db/migrate/20230829142122_create_recommendations.rb
+++ db/migrate/20230829142122_create_recommendations.rb
@@ -1,8 +1,8 @@
 class CreateRecommendations < ActiveRecord::Migration[7.0]
   def change
     create_table :recommendations do |t|
-      t.references :giver, null: false, foreign_key: true
-      t.references :receiver, null: false, foreign_key: true
+      t.references :giver, null: false, foreign_key: { to_table: 'users' }
+      t.references :receiver, null: false, foreign_key: { to_table: 'users' }
       t.references :item, null: false, foreign_key: true
       t.string :comment
 
--- db/migrate/20230829142337_create_messages.rb
+++ db/migrate/20230829142337_create_messages.rb
@@ -1,9 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.string :recommendation
-      t.string :references
       t.references :user, null: false, foreign_key: true
+      t.references :recommendation, null: false, foreign_key: true
 
       t.timestamps
     end
