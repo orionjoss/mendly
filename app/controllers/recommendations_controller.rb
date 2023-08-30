55a5abae6c418bd95608689fe7c63e6df4d4f9e6
jorge marques <jorgetmarques@gmail.com>
2023-08-30 13:23:21 +0100

    recommendations working: show, create, edit, delete. with forms

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -3,5 +3,46 @@ class RecommendationsController < ApplicationController
     @recommendation = Recommendation.find(params[:id])
   end
 
-  
+  def new
+    @recommendation = Recommendation.new
+  end
+
+  def create
+    @recommendation = Recommendation.new(recommendation_params)
+    @recommendation.receiver = current_user
+    # @recommendation.user = recommendation.receiver
+    # @recommendation.item = Item.find(params[:recommendation_id, :item_id])
+
+    if @recommendation.save
+      redirect_to recommendation_path(@recommendation)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @recommendation = Recommendation.find(params[:id])
+  end
+
+  def update
+    @recommendation = Recommendation.find(params[:id])
+    if @recommendation.update(recommendation_params)
+      redirect_to recommendation_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recommendation = Recommendation.find(params[:id])
+    @recommendation.destroy
+    redirect_to home_path
+  end
+
+  private
+
+
+  def recommendation_params
+    params.require(:recommendation).permit(:item_id, :comment, :giver_id, :receiver_id)
+  end
 end
