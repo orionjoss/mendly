1722f1b06c2dfcf42ba963e48e4740e1862992c4
jorge marques <jorgetmarques@gmail.com>
2023-08-31 15:59:41 +0100

    home-front-end. bottom of the page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @recommendations = Recommendation.all[0..1]
+    @recommendations = Recommendation.all[0..20]
     @lists = List.all
   end
+
 end
