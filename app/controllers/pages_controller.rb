8574fcecfbdc23fc57ff512aaea338857dbe0609
orionjoss <orionjoss@gmail.com>
2023-09-04 17:34:45 +0100

    created the chat page, show and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,14 @@ class PagesController < ApplicationController
     @categories = Category.all
     @givers = User.where.not(id: current_user.id)
   end
+
+  def chat
+    # when user clicks on "message"
+    # chat page opens
+    # chat will have "pinned message" - ask user "what do you want to do"
+    # user write message with "new recommendation"
+    # chatbot sends message saying "recommendation added successfully"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   end
   resources :users, only: %i[show]
 
+  get "/chat", to: "pages#chat"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
