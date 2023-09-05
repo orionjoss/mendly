4bee1bce15c6f434063dc380b4126ae5e03745b0
orionjoss <orionjoss@gmail.com>
2023-09-05 01:56:58 +0100

    generated new model for chatroom and changed migration for messages

--- db/migrate/20230904221650_create_messages.rb
+++ db/migrate/20230904221650_create_messages.rb
@@ -1,8 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
-      t.references :user, null: false, foreign_key: true
+      t.string :content
       t.references :recommendation, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :chatroom, null: false, foreign_key: true
 
       t.timestamps
     end
