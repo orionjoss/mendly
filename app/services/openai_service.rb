b1ba724e3f20923efb1da8218f31ce149696588e
Hugo Pereira <hugop1997@gmail.com>
2023-09-06 16:25:25 +0100

    Search complete including item title genre and Giver nickname

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -11,11 +11,11 @@ class OpenaiService
   def call
     response = client.chat(
       parameters: {
-          model: "gpt-3.5-turbo", # Required.
-          messages: [{ role: "user", content: prompt }], # Required.
-          temperature: 0.7,
-          stream: false,
-					max_tokens: 100 # might want to check this
+        model: "gpt-3.5-turbo", # Required.
+        messages: [{ role: "user", content: prompt }], # Required.
+        temperature: 0.7,
+        stream: false,
+        max_tokens: 100 # might want to check this
       }
     )
     # you might want to inspect the response and see what the api is giving you
